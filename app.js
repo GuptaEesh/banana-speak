91c61352f752ef4eeb4923d90f380122827dce49
GuptaEesh <p1r8a5t7ibha@gmail.com>
2021-09-11 12:16:34 +0530

    feat: many new things added

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
-var btnSelector=document.querySelector("#btn");
-function clickEventHandler(){
-    alert("Hi")
+var btnSelector = document.querySelector("#btn");
+var txtInput = document.querySelector("#txt-input");
+var getinfo = document.querySelector("#get-info");
+
+// var masterUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var serverUrl = "	https://api.funtranslations.com/translate/minion.json";
+
+function getTranslatedUrl(text) {
+    return serverUrl + "?" + "text=" + text;
+}
+
+function errorHandler(error) {
+    console.log("Error Occured", error);
+    alert("Something wrong with server try again after sometime");
+}
+
+function clickEventHandler() {
+    var inputText = txtInput.value;
+    fetch(getTranslatedUrl(inputText)).then(response => response.json()).then(json => {
+        var trans = json.contents.translated;
+        getinfo.innerText = trans;
+    }).catch(errorHandler);
+
 };
-btnSelector.addEventListener("click",clickEventHandler);
+btnSelector.addEventListener("click", clickEventHandler);
